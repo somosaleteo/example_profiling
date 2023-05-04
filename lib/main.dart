ac9fd6c733fa095bf33c11e1804bdedc684e061e
Camilo Agudelo <camiiloagudelo92@gmail.com>
2023-05-04 16:18:25 -0500

    Merge tips branch

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:example_profiling/tips/tips_page.dart';
 import 'package:example_profiling/ui/pages/example/high_performance_page.dart';
 import 'package:example_profiling/ui/pages/example/low_performance_page.dart';
 import 'package:example_profiling/ui/pages/my_home_page.dart';
@@ -21,6 +22,7 @@ class MyApp extends StatelessWidget {
         'home': (context) => const MyHomePage(),
         'lowPerformance': (context) => const LowePerformancePage(),
         'highPerformance': (context) => const HighPerformancePage(),
+        'tips': (context) => const TipsPage(),
       },
       initialRoute: 'home',
     );
